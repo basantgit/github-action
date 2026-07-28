6fd2792a0ef2c29db08a2ca0695a98062672258a
basantgit <basantmohanty7@gmail.com>
2026-07-28 10:33:21 +0530

    code added

---
+++ src/math_operation.py
@@ -0,0 +1,25 @@
+def add_numbers(a, b):
+    """
+    Adds two numbers together.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The sum of the two numbers.
+    """
+    return a + b
+
+def subtract_numbers(a, b):
+    """
+    Subtracts the second number from the first number.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The difference of the two numbers.
+    """
+    return a - b
